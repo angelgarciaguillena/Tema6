07cf3fb21ff16b9b8dbc2df79de40a1e5cb1b2c4
angelgarciaguillena <angel.garcia@iesnervion.es>
2025-04-23 10:41:13 +0200

    Ejercicios

--- src/boletin1/ejercicio5/Ejercicio5.java
+++ src/boletin1/ejercicio5/Ejercicio5.java
@@ -1,29 +1,32 @@
-package boletin1.ejercicio4;
+package boletin1.ejercicio5;
 
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Ejercicio4 {
+public class Ejercicio5 {
 	
-	public static final String FICHERO = "src\\boletin1\\ejercicio4\\Palabras";
+	public static final String FICHERO = "src\\boletin1\\ejercicio5\\datos.txt";
+	
 	public static void main(String[] args) {
-		
-		String palabra = "";
-		
+		String nombre;
+		int edad;
 		Scanner sc = new Scanner(System.in);
-		
 		try(BufferedWriter bw = new BufferedWriter(new FileWriter(FICHERO, true))) {
 			
-			do {
-				System.out.println("Introduce una palabra");
-				palabra = sc.nextLine();
-				bw.write(palabra);
-				bw.newLine();
-			}while(!palabra.equalsIgnoreCase("fin"));
+			System.out.println("Introduce el nombre");
+			nombre = sc.nextLine();
 			
-			bw.close();
+			System.out.println("Introduce la edad");
+			edad = sc.nextInt();
+			
+			bw.write(nombre + " " + edad);
+			
+			bw.newLine();
+			
+			
+
 		} catch (IOException e) {
 			System.out.println("Error: No se ha podido escribir en el fichero " + e.getMessage());
 		}
